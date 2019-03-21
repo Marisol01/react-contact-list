a333b4371fcf17d90e79700f4be15c35900a2d48
Marisoln Araujo <u167272@gmail.com>
2019-03-20 20:54:34 -0600

    end of lecture

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Header, Button, Icon, Segment } from 'semantic-ui-react';
+import Contacts from './contacts/Contacts';
+import ContactForm from './contacts/ContactForm';
 
 class App extends Component {
+  state = { 
+    contacts: [
+      {id: 1, firstName: 'Bob', phone: '801-123-1234'},
+      {id: 2, firstName: 'Rob', phone: '801-123-1424'},
+      {id: 3, firstName: 'Kob', phone: '801-123-1432'}
+    ],
+    showForm: true
+  }
+
+  toggleForm = () => {
+    this.setState({ showForm: !this.state.showForm })
+  }
+
+  addContact = (contactData) => {
+    let contact = { id: this.getId(), ...contactData }
+    this.setState({ contacts: [contact, ...this.state.contacts] })
+  }
+
+  getId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1)
+  }
+
+  removeContact = (id) => {
+    const contacts = this.state.contacts.filter( contact => {
+      if (contact.id !== id )
+        return contact
+    })
+    this.setState({ contacts: [...contacts]})
+  }
+
   render() {
+    const { contacts, showForm } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Header as='h1'>React Contact List</Header>
+        <Segment>
+          <Button icon color='brown' onClick={this.toggleForm}>
+            <Icon name={ showForm ? 'angle double up' : 'angle double down' } />
+          </Button>
+          { showForm ? <ContactForm add={this.addContact} /> : null } 
+        </Segment>
+        <br />
+        <Contacts contacts={contacts} remove={this.removeContact} />
+      </Container>
     );
   }
 }
